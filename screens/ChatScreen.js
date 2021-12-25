b508641fe2b3bcd41cea8681f58d940a9b1d5734
nipun chawla <nipun.chawla@braincorp.com>
2021-12-25 02:24:35 -0800

    screen2 -- chat screen

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-
+import { View, Text, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/core';
 const ChatScreen = () => {
     return (
         <View>
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-
+import { View, Text, Button } from 'react-native'
+import { useNavigation } from '@react-navigation/core';
 const HomeScreen = () => {
+    const navigation = useNavigation();
     return (
         <View>
-            <Text>I am the HomeScreen --</Text>
+            <Text  > I am the HomeScreen --</Text>
+            <Button onPress={()=>navigation.navigate("Chat")} title="Go to ChatScreen"/>
         </View>
     )
 }
